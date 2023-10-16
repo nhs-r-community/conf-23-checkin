a85ab2b8ea665594050859d11cb28123dbe233e7
Tom Jemmett <thomas.jemmett@nhs.net>
2023-10-16 13:38:41 +0100

    adds a section on desktop devices to show the list of last checked-in people (for checking off on a paper copy)

--- frontend/src/Attendees.js
+++ frontend/src/Attendees.js
@@ -4,6 +4,7 @@ import useWebSocket from 'react-use-websocket';
 import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import InputGroup from 'react-bootstrap/InputGroup';
 
@@ -21,20 +22,36 @@ const Attendees = ({ checkIn, date }) => {
 
   const { lastMessage } = useWebSocket(process.env.REACT_APP_WS_URI);
 
+  const [messageLog, setMessageLog] = useState([]);
+
   const getN = () => Object.values(allAttendees.current).reduce(
     (a, v) => a + (v.checked_in > 0), 0
   );
   const getD = () => Object.keys(allAttendees.current).length;
 
+  const addToMessageLog = x => ml => {
+    for (let i = 0; i < ml.length; i++) {
+      if (ml[i].name == x.name) {
+        ml[i] = x
+        return ml;
+      }
+    }
+
+    ml.push(x);
+    return ml;
+  }
+
   useEffect(() => {
     if (!lastMessage) return;
 
     try {
-      const { id, checked_in } = JSON.parse(lastMessage.data);
+      const { id, checked_in, name } = JSON.parse(lastMessage.data);
 
       const v = new Date(checked_in).getTime() / 1000;
 
       allAttendees.current[id].checked_in = v;
+
+      setMessageLog(addToMessageLog({ name, checked_in }));
     } catch (e) { }
   }, [lastMessage]);
 
@@ -68,62 +85,74 @@ const Attendees = ({ checkIn, date }) => {
     setAttendees(f);
   }, [search, checkCheckedIn, checkNotCheckedIn, fuse]);
 
-  const handleSearch = (s) => {
-    setSearch(s);
-  }
-  const handleCheckCheckedIn = () => {
-    setCheckCheckedIn(!checkCheckedIn);
-  }
-  const handleCheckNotCheckedIn = () => {
-    setCheckNotCheckedIn(!checkNotCheckedIn);
-  }
+  const handleSearch = s => setSearch(s);
+  const handleCheckCheckedIn = () => setCheckCheckedIn(!checkCheckedIn);
+  const handleCheckNotCheckedIn = () => setCheckNotCheckedIn(!checkNotCheckedIn);
+  const handleClearLastCheckedIn = () => setMessageLog([]);
 
   return (
     <>
       <h2>Attendees List</h2>
       <p>Checked in: {getN()} / {getD()}</p>
-      <Form.Group>
-        <InputGroup className="mb-3">
-          <InputGroup.Text id="basic-addon1">filter attendees</InputGroup.Text>
-          <Form.Control
-            placeholder="Search"
-            aria-label="Search"
-            aria-describedby="basic-addon1"
-            onChange={e => handleSearch(e.target.value)}
-          />
-        </InputGroup>
-      </Form.Group>
-      <div className="mb-3">
-        <Form.Check
-          inline
-          label="Checked In"
-          name="checkedingroup"
-          type="checkbox"
-          id="checkedin"
-          checked={checkCheckedIn}
-          onChange={handleCheckCheckedIn}
-        />
-        <Form.Check
-          inline
-          label="Not-Checked In"
-          name="checkedingroup"
-          type="checkbox"
-          id="notcheckedin"
-          checked={checkNotCheckedIn}
-          onChange={handleCheckNotCheckedIn}
-        />
-      </div>
+      <div className="row">
+        <div className="col-lg">
+          <Form.Group>
+            <InputGroup className="mb-3">
+              <InputGroup.Text id="basic-addon1">filter attendees</InputGroup.Text>
+              <Form.Control
+                placeholder="Search"
+                aria-label="Search"
+                aria-describedby="basic-addon1"
+                onChange={e => handleSearch(e.target.value)}
+              />
+            </InputGroup>
+          </Form.Group>
+          <div className="mb-3">
+            <Form.Check
+              inline
+              label="Checked In"
+              name="checkedingroup"
+              type="checkbox"
+              id="checkedin"
+              checked={checkCheckedIn}
+              onChange={handleCheckCheckedIn}
+            />
+            <Form.Check
+              inline
+              label="Not-Checked In"
+              name="checkedingroup"
+              type="checkbox"
+              id="notcheckedin"
+              checked={checkNotCheckedIn}
+              onChange={handleCheckNotCheckedIn}
+            />
+          </div>
 
-      <ul className="fa-ul">
-        {
-          attendees.map(a => (
-            <li className="attendee" key={a.id} onClick={() => checkIn(a.id)}>
-              <span className="fa-li"><FontAwesomeIcon icon={a.checked_in > 0 ? faCheck : faXmark} /></span>
-              <strong>{a.name}</strong> <i>{a.email}</i>
-            </li>
-          ))
-        }
-      </ul>
+          <ul className="fa-ul">
+            {
+              attendees.map(a => (
+                <li className="attendee" key={a.id} onClick={() => checkIn(a.id)}>
+                  <span className="fa-li"><FontAwesomeIcon icon={a.checked_in > 0 ? faCheck : faXmark} /></span>
+                  <strong>{a.name}</strong> <i>{a.email}</i>
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+        <div className="col-lg-3 offset-lg-3 d-none d-lg-block d-xl-block">
+          <h3>Last Checked-in</h3>
+          <Button variant="success" onClick={handleClearLastCheckedIn}>Clear</Button>
+          <ul>
+            {
+              messageLog.map(m => (
+                <li key={m.name} style={{ textDecoration: m.checked_in === "0" ? "line-through" : "none" }}>
+                  {m.name}
+                </li>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
     </>
   );
 }
